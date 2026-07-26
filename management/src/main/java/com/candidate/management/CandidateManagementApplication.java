683c78e0de7e6d6b765e5b74a2a911db3dab9273
trainingsshiva <trainings.shiva@gmail.com>
2026-07-26 14:46:01 +0530

    SpringBootApplication changes

--- management/src/main/java/com/candidate/management/CandidateManagementApplication.java
+++ management/src/main/java/com/candidate/management/CandidateManagementApplication.java
@@ -4,7 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(
+    exclude = {
+        org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration.class
+    }
+)
 @EnableScheduling
 public class CandidateManagementApplication {
     public static void main(String[] args) {
